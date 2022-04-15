b365ecdd075021c14a9db3aacaf27b5edb55356a
vjmarteau <valentinmarteau7@gmail.com>
2022-04-15 11:13:47 +0200

    Update fkt to process multiple pdfs
    As of now you need to individually download the Routendatenbank pdfs
    (https://www.kletterzentrum-innsbruck.at/index.php?id=235&no_cache=1)
    in chunks of 100 Climbing routes, the max number you can display on the
    website. Until this is fixed I modified the original function to look for
    all downloaded pdfs, load, read, and process them to output a single
    tibble.

--- lib/pdf_to_tibble.R
+++ lib/pdf_to_tibble.R
@@ -1,50 +1,68 @@
 # Takes the KI-Routendatenbank pdf from https://www.kletterzentrum-innsbruck.at/index.php?id=235&no_cache=1 and converts it to a tibble
 # Requires packages "tidyverse" and "pdftools"
 
-pdf_to_tibble <- function(path_to_file) {
+pdf_to_tibble <- function(path_to_pdfs) {
   
-# Read pdf lines and remove white space
-metadat <- pdf_text(path_to_file) |>
-  readr::read_lines() |>
-  str_squish() |>
-  str_subset(".+")
+  # Read pdf lines and remove lines with less than 4 characters
+  filenames <- list.files(path_to_pdfs, recursive = TRUE, full.names = TRUE, pattern = '.pdf$')
   
-# Remove lines with less than 4 characters, extract colnames in first line
-text <- metadat[str_count(metadat, " ") > 4][-1]
-colnames <- metadat[1] |> stringr::str_split(" ") |> as_vector()
-colnames <- c(colnames[1:2], Reduce(paste, colnames[3:4]), colnames[5:6])
+  metadat <- map_df(filenames, ~ tibble(txt = pdf_text(.x) |>
+                                          readr::read_lines() |>
+                                          str_squish() |>
+                                          str_subset(".+")))
   
-# Check number of words
-words <- text |>
-  stringr::str_split(" ") |>
-  map(length) |>
-  unlist()
+  # Extract colnames in first line and all th rest
+  colnames <- metadat |>
+    slice_head() |> stringr::str_split(" ") |> unlist()
+  colnames <- c(colnames[1:2], Reduce(paste, colnames[3:4]), colnames[5:6])
   
-# Split character vector at spaces and put into columns
-## Approach from https://localcoder.org/using-strsplit-and-subset-in-dplyr-and-mutate
-metadat <- text |>
-  tibble() |>
-  filter(words > 6) |>
-  mutate(tmp_chunks = stringr::str_split(text, fixed(" "),  n = 6)) |>
-  mutate(!!colnames[1] := map_chr(tmp_chunks, 1),
-          !!colnames[2] := map_chr(tmp_chunks, 2),
-          !!colnames[3] := map_chr(tmp_chunks, 3),
-          !!colnames[4] := map_chr(tmp_chunks, 4),
-          !!colnames[5] := map_chr(tmp_chunks, 5),
-          !!colnames[6] := map_chr(tmp_chunks, 6)) |>
-  select(-tmp_chunks) |>
-  unite(Sektor, c("Sektor", "Routensetzer")) |>
-  rename(!!colnames[5] := "NA" ) |> 
-  add_row(text |>
-            tibble() |>
-            filter(words < 7) |>
-            mutate(tmp_chunks = stringr::str_split(text, fixed(" "),  n = 5)) |>
-            mutate(!!colnames[1] := map_chr(tmp_chunks, 1),
-                    !!colnames[2] := map_chr(tmp_chunks, 2),
-                    !!colnames[3] := map_chr(tmp_chunks, 3),
-                    !!colnames[4] := map_chr(tmp_chunks, 4),
-                    !!colnames[5] := map_chr(tmp_chunks, 5)) |>
-            select(-tmp_chunks))
+  metadat <- metadat |>
+    filter(str_count(metadat$txt, " ") > 4) |>
+    distinct() |>
+    slice(-1)
   
-return(metadat)
+  # Check number of words
+  words <- metadat |>
+    pull(txt) |>
+    stringr::str_split(" ") |>
+    map(length) |>
+    unlist()
+  
+  # Split character vector at spaces and put into columns
+  ## Approach from https://localcoder.org/using-strsplit-and-subset-in-dplyr-and-mutate
+  
+  textA <- metadat |> filter(words > 6) |> pull(txt)
+  textB <- metadat |> filter(words < 7) |> pull(txt)
+  
+  meta <- metadat |>
+    filter(words > 6) |>
+    mutate(tmp_chunks = stringr::str_split(textA, fixed(" "),  n = 6)) |>
+    mutate(!!colnames[1] := map_chr(tmp_chunks, 1),
+           !!colnames[2] := map_chr(tmp_chunks, 2),
+           !!colnames[3] := map_chr(tmp_chunks, 3),
+           !!colnames[4] := map_chr(tmp_chunks, 4),
+           !!colnames[5] := map_chr(tmp_chunks, 5),
+           !!colnames[6] := map_chr(tmp_chunks, 6)) |>
+    select(-tmp_chunks) |>
+    unite(Sektor, c("Sektor", "Routensetzer")) |>
+    rename(!!colnames[5] := "NA" ) |>
+    add_row(metadat |>
+              tibble() |>
+              filter(words < 7) |>
+              mutate(tmp_chunks = stringr::str_split(textB, fixed(" "),  n = 5)) |>
+              mutate(!!colnames[1] := map_chr(tmp_chunks, 1),
+                     !!colnames[2] := map_chr(tmp_chunks, 2),
+                     !!colnames[3] := map_chr(tmp_chunks, 3),
+                     !!colnames[4] := map_chr(tmp_chunks, 4),
+                     !!colnames[5] := map_chr(tmp_chunks, 5)) |>
+              select(-tmp_chunks))
+  
+  meta <- meta |>
+    mutate_at(4, dmy) |>
+    rename(date_route_set = `Gebaut am`) |>
+    mutate(Linie = as.integer(Linie)) |>
+    arrange(Linie) |>
+    relocate(1, .after = last_col())
+  
+  return(meta)
 }
