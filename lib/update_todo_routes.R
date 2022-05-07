b4a381cb09e4de840cbeeba1c9b04868793d5ad5
vjmarteau <valentinmarteau7@gmail.com>
2022-05-07 15:12:09 +0200

    Add column for date last attempted

--- lib/update_todo_routes.R
+++ lib/update_todo_routes.R
@@ -13,15 +13,28 @@ red_point <- latest |>
   filter(person == person) |>
   filter(topped_out == "yes") |>
   unite(id, c("route_number", "grade"), remove = FALSE)
-
+  
+attempted <- latest |>
+  filter(person == person) |>
+  filter(topped_out == "no") |>
+  unite(id, c("route_number", "grade"), remove = FALSE) |>
+  select(id, date) |>
+  group_by(id) |>
+  filter(date == max(date))
+  
 # Exlude too easy/hard routes
 missing <- meta |>
   filter(!meta$id %in% red_point$id) |>
   filter(!Schwierigkeitsgrad %in% c("3a","4a", "4b", "4c", "5a", "5a+", "5b", "5b+", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a", "9a+")) |>
-  select(Linie, Schwierigkeitsgrad, Sektor, Routensetzer) |>
+  select(id, Linie, Schwierigkeitsgrad, Sektor, Routensetzer) |>
   arrange(Schwierigkeitsgrad, Linie)
-
+  
+# Add column with date of last_attempt
+to_do <- left_join(missing, attempted, by = "id") |>
+  select(Linie, Schwierigkeitsgrad, date, Sektor, Routensetzer) |>
+  rename("last_attempt" = "date")
+  
 # Write to_do routes to google sheets 
-sheet_write(data = missing, ss = URL,
+sheet_write(data = to_do, ss = URL,
             sheet = paste0(person, "_to_do"))
 }
