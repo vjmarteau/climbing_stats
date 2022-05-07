6f74840f5a7e445984100dec1106fb6aac18b311
vjmarteau <valentinmarteau7@gmail.com>
2022-05-07 16:27:46 +0200

    Add number of attempts
    closes #5

--- lib/update_todo_routes.R
+++ lib/update_todo_routes.R
@@ -8,32 +8,44 @@ latest <- left_join(sheets, metadata, by = "id") |>
   filter(status == T) |>
   select(route_number, grade, colour, date_route_set, date, person, feeling, topped_out, Sektor, Routensetzer) |>
   unite(id, c("route_number", "grade", "colour"), remove = FALSE)
-  
+
 red_point <- latest |>
   filter(person == person) |>
   filter(topped_out == "yes") |>
   unite(id, c("route_number", "grade"), remove = FALSE)
-  
+
 attempted <- latest |>
   filter(person == person) |>
   filter(topped_out == "no") |>
   unite(id, c("route_number", "grade"), remove = FALSE) |>
   select(id, date) |>
   group_by(id) |>
-  filter(date == max(date))
-  
+  mutate(attempts = n_distinct(date)) |>
+  select(id, date, attempts) |>
+  filter(date == max(date)) |>
+  mutate(attempts = na_if(attempts, 1L))
+
 # Exlude too easy/hard routes
 missing <- meta |>
   filter(!meta$id %in% red_point$id) |>
   filter(!Schwierigkeitsgrad %in% c("3a","4a", "4b", "4c", "5a", "5a+", "5b", "5b+", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a", "9a+")) |>
-  select(id, Linie, Schwierigkeitsgrad, Sektor, Routensetzer) |>
-  arrange(Schwierigkeitsgrad, Linie)
-  
+  select(id, Linie, Schwierigkeitsgrad, Sektor, Routensetzer)
+
 # Add column with date of last_attempt
 to_do <- left_join(missing, attempted, by = "id") |>
-  select(Linie, Schwierigkeitsgrad, date, Sektor, Routensetzer) |>
+  select(Linie, Schwierigkeitsgrad, date, attempts, Sektor, Routensetzer) |>
   rename("last_attempt" = "date")
-  
+
+# Append number of attempts == 1
+to_do1 <- to_do |>
+  filter(!is.na(last_attempt)) |>
+  mutate(attempts = replace_na(attempts, 1L))
+to_do2 <- to_do |>
+  filter(is.na(last_attempt))
+
+to_do <- bind_rows(to_do1, to_do2) |>
+  arrange(Schwierigkeitsgrad, Linie)
+
 # Write to_do routes to google sheets 
 sheet_write(data = to_do, ss = URL,
             sheet = paste0(person, "_to_do"))
