95582feac97ce43c3270dee3245900979328710b
vjmarteau <valentinmarteau7@gmail.com>
2022-05-12 15:58:39 +0200

    Fix update_todo_routes.R function bug
    I noticed missing routes in the google-sheets to do list that were not
    listed although I knew I had not climbed them. The problem arose when
    the "attempted" column was computed (and subsequently filtered), as in
    some cases a route was attempted by a single climber. One solution might
    be to additionally distinguish attempts by the climber (append label).
    I went for an "easier" solution by adding the missing routes back at the
    end.

--- lib/update_todo_routes.R
+++ lib/update_todo_routes.R
@@ -1,43 +1,47 @@
-# Function to compute to do list of current Ki Routes and write to googlesheets with respective name of person
+# Function to compute to do list of current Ki Routes and write to google-sheets with respective name of climber
 
-update_todo_routes <- function(sheets, metadata, person, URL) {
+update_todo_routes <- function(metadata, sheets, climber, URL) {
 
-# Remove routes that are not there any more according to "date_set"
-latest <- left_join(sheets, metadata, by = "id") |>
-  mutate(status = date_route_set < date) |>
-  filter(status == T) |>
-  select(route_number, grade, colour, date_route_set, date, person, feeling, topped_out, Sektor, Routensetzer, Ki_colour) |>
-  unite(id, c("route_number", "grade", "colour"), remove = FALSE)
+# Exlude too easy/hard routes and drop factor levels
+metadata <- metadata |>
+  filter(!Schwierigkeitsgrad %in% c("3a","4a", "4b", "4c", "5a", "5a+", "5b", "5b+", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a", "9a+")) |>
+  mutate(Schwierigkeitsgrad = fct_drop(Schwierigkeitsgrad))
 
-red_point <- latest |>
-  filter(person == person) |>
-  filter(topped_out == "yes") |>
-  unite(id, c("route_number", "grade"), remove = FALSE)
+# Remove routes that are not there any more according to date_set (Gebaut_am)
+df <- left_join(metadata, sheets, by = "id") |>
+  mutate(attempted = ifelse(is.na(topped_out), "not_attempted", "attempted")) |>
+  mutate(latest = Gebaut_am <= date) |>
+  mutate(latest = replace_na(latest, TRUE))
 
-attempted <- latest |>
-  filter(person == person) |>
-  filter(topped_out == "no") |>
-  unite(id, c("route_number", "grade"), remove = FALSE) |>
-  select(id, date) |>
+person <- df |>
+  filter(latest == T, person == climber | attempted == "not_attempted")
+
+curr_red_point <- person |>
+  filter(topped_out == "yes")
+
+# Add column with date of last_attempt + number of attempts
+attempted <- person |>
+  filter(topped_out == "no" | attempted == "attempted") |>
+  filter(!id %in% curr_red_point$id) |>
   group_by(id) |>
   mutate(attempts = n_distinct(date)) |>
-  select(id, date, attempts) |>
   filter(date == max(date)) |>
-  distinct()
+  distinct() |>
+  ungroup()
 
-# Exlude too easy/hard routes
-missing <- meta |>
-  filter(!meta$id %in% red_point$id) |>
-  filter(!Schwierigkeitsgrad %in% c("3a","4a", "4b", "4c", "5a", "5a+", "5b", "5b+", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a", "9a+")) |>
-  select(id, Linie, Schwierigkeitsgrad, Sektor, Routensetzer, Ki_colour)
+not_attempted <- person |>
+  filter(attempted == "not_attempted")
 
-# Add column with date of last_attempt
-to_do <- left_join(missing, attempted, by = "id") |>
-  select(Linie, Schwierigkeitsgrad, Ki_colour, date, attempts, Sektor, Routensetzer) |>
-  rename("last_attempt" = "date", "Farbe" = "Ki_colour") |>
+tmp_meta <- metadata |>
+  filter(!metadata$id %in% person$id)
+
+to_do <- bind_rows(attempted, not_attempted, tmp_meta) |>
+  distinct(id, .keep_all = TRUE) |>
+  select(Linie, Schwierigkeitsgrad, Farbe, date, attempts, Sektor, Routensetzer, feeling, Comments) |>
+  rename("last_attempt" = "date", "Versuche" = "attempts") |>
   arrange(Linie, Schwierigkeitsgrad)
 
 # Write to_do routes to google sheets
 sheet_write(data = to_do, ss = URL,
-            sheet = paste0(person, "_to_do"))
+            sheet = paste0(climber, "_to_do"))
 }
